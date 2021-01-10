40624692faa9fc9a26d26e07e68c1ab6f0d274f9
TrungNT <trungnt2@gmail.com>
2021-01-10 16:08:39 +0700

    first

---
+++ src/Components/TodoApp.js
@@ -0,0 +1,15 @@
+import React from "react" ;
+
+class TodoApp extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <h1> Hello from React App</h1>
+                <p> I am react Developer </p>
+            </div>
+        );
+    }
+}
+
+export default TodoApp;
---
+++ src/layout/TodoHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TodoHeader extends React.Component{
+    render(){
+        return(
+            <header>
+                <h1> </h1>
+            </header>
+        );
+
+    }
+        
+};
