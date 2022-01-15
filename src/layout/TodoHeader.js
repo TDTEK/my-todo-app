6373a0bc6332cf6e89c68591585d4c065a42e04e
Trung Nguyen <trungnt2@gmail.com>
2022-01-16 01:43:05 +0700

    test thu

--- src/layout/TodoHeader.js
+++ src/layout/TodoHeader.js
@@ -3,8 +3,8 @@ import React from 'react';
 class TodoHeader extends React.Component{
     render(){
         return(
-            <header>
-                <h1> </h1>
+            <header> 
+                <h1> </h1> 
             </header>
         );
 
